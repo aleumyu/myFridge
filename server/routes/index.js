bda270930d261636c7ff4dbabf3a9364d6e219e5
ALeum Yang Urfels <aleum@ALeums-MacBook-Pro.local>
2019-06-02 17:19:58 +0200

    fitst commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const db = require("../model/helper");
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/api/v1/items', (req, res, next) => { //api/v1/items?is_fridge===1) then query params
+  db('SELECT * FROM items ORDER BY date ASC;')
+    .then(results => {
+      if (results.error) { // where .error and .data come from? JS? Express?
+        res.status(404).send(results.error);
+      }
+      console.log('results: ' + JSON.stringify(results.data));
+      res.send(results.data);
+    })  // why we don-t throw Error?? actually it does not work
 });
 
 module.exports = router;
